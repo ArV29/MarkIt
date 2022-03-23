f6fb7d880c565d8ff26f4ff9fe66f5c18ca1d1fa
ArV <aryanrajvaidya@gmail.com>
2022-03-23 23:09:22 +0530

    Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:mark_it/app.dart';
+import 'package:mark_it/FrontEnd/Login.dart';
 
 void main() {
-  runApp(const Homepage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginScreen());
+  }
 }
