3f7bfd0b2ae24dcd95290e2a3a2eb0e122572072
ArV <aryanrajvaidya@gmail.com>
2022-03-23 22:31:03 +0530

    Geo Test

--- lib/app.dart
+++ lib/app.dart
@@ -1,50 +1,45 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import './packages/geo/geo_compare.dart';
 import './packages/geo/geo_return.dart';
 
-class Homepage extends StatefulWidget{
-  createState(){
-    return HomepageState();
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  createState() {
+    return HomePageState();
   }
 }
 
-class HomePageState extends State<Homepage>{
-
+class HomePageState extends State<Homepage> {
   Widget build(context) {
-      return MaterialApp(
-        home: Scaffold(
-          body:
-          z
-          appBar: AppBar(
-            title: Text(),
-          ),
+    return MaterialApp(
+      home: Scaffold(
+        body: compareButton(),
+        appBar: AppBar(
+          title: const Text("Hello"),
         ),
-      );
-    }
+      ),
+    );
+  }
 }
 
 Widget compareButton() {
-  Position employee = await _determinePosition();
-  Position organisation;
-  organisation.latitude=30.765616;
-  organisation.longitude=76.753638;
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: Colors.purple,
-        padding: EdgeInsets.all(15.0),
-        textStyle: TextStyle(
-          color: Colors.white10,
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-        ),
+  return ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      primary: Colors.purple,
+      padding: const EdgeInsets.all(15.0),
+      textStyle: const TextStyle(
+        color: Colors.white10,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
       ),
-      onPressed: () {
-        print(compare(organisation, employee))
-      },
-      child: Text('Submit'),
-    );
-  }
-
+    ),
+    onPressed: () async {
+      Position employee = await determinePosition();
+      print(compare(employee));
+    },
+    child: const Text('Submit'),
+  );
+}
